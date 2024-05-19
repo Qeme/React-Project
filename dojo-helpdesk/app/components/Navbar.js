1f327b88df39115757d27a050c2822dd8b6f7c73
Qeme_AX2 <qemeofficial07@gmail.com>
2024-05-19 12:15:53 +0800

    Apply some font style + Add CSS + Use Image tag to import and use image

--- dojo-helpdesk/app/components/Navbar.js
+++ dojo-helpdesk/app/components/Navbar.js
@@ -1,9 +1,20 @@
 // import Link from next framework to use similar like a anchor
+import Image from "next/image";
 import Link from "next/link";
+// import Image here as logo
+import Logo from "./dojo-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
+        {/* call image Tag */}
+        <Image 
+            src={Logo}
+            alt="Dojo Helpdesk Logo"
+            width={70}
+            quality={100}
+            placeholder="blur"
+        />
         <h1>Dojo Helpdesk</h1>
         <Link href="/">Dashboard</Link>
         <Link href="/tickets">Tickets</Link>
