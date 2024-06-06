3ee4e00af44b22b2c2bfa8007c927871d9a5ca7a
Qeme_AX2 <qemeofficial07@gmail.com>
2024-06-06 22:50:31 +0800

    When the user refresh the page, the state becomes null however in localStorage it remains there. So the solution is to add useEffect that run at least once to check whether is there any user data in it, if yes dispatch LOGIN if not, means both are null

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,14 @@
 // import Link to do link components
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // create a Navbar function
 const Navbar = () => {
   // call for the logout component
   const { logout } = useLogout();
+  // call for the user context from useAuthContext
+  const { user } = useAuthContext();
 
   // create a function to call for the logout function from useLogout custom hook
   const handleClick = () => {
@@ -20,13 +23,19 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
+          {user && (
           <div>
+            {/* add user email here beside the logout button */}
+            <span>{user.email}</span>
             <button onClick={handleClick}>Logout</button>
           </div>
-          <div>
+          )}
+          {!user && (
+            <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
           </div>
+          )}
         </nav>
       </div>
     </header>
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 // import createContext from react
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 // declare a constant variable to hold the createContext funtion
 // export it as well
@@ -29,6 +29,17 @@ export const AuthContextProvider = ({ children }) => {
 
     // apply useReducer here the useState, but we need to put funct as first argument to handle the state
     const [state, dispatch] = useReducer(authReducer, { user  : null } )
+
+    // add useEffect() that run once, means it will check the localStorage first (because it doesnt disappear when user refresh the page/close the tab)
+    useEffect(() => {
+        // grab the data from the localStorage if there is one
+        const user = JSON.parse(localStorage.getItem("user"))
+
+        // if it really exist, we call back the dispatch to LOGIN, basically set up back the user context data
+        if(user){
+            dispatch({type: 'LOGIN', payload: user})
+        }
+    }, []) 
 
     // print the console.log to see the user state for debugging later on
     console.log('Authentication State: ',state)
