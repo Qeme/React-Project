cfec2eb51745eac7f21887e6f1592b37c7dc9227
Qeme_AX2 <qemeofficial07@gmail.com>
2024-06-06 19:40:51 +0800

    Apply Logout custom hook by making the user state to null and remove user data from localStorage

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,25 +1,37 @@
 // import Link to do link components
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 // create a Navbar function
-const Navbar = () =>{
-    return (
-        <header>
-            <div className="container">
-            {/* now create a link to the Home */}
-            <Link to='/'>
-                <h1>Workout Buddy</h1>
-            </Link>
-            <nav>
-                <div>
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Signup</Link>
-                </div>
-            </nav>
-            </div>
-        </header>
-    )
-}
+const Navbar = () => {
+  // call for the logout component
+  const { logout } = useLogout();
+
+  // create a function to call for the logout function from useLogout custom hook
+  const handleClick = () => {
+    logout();
+  };
+
+  return (
+    <header>
+      <div className="container">
+        {/* now create a link to the Home */}
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+        <nav>
+          <div>
+            <button onClick={handleClick}>Logout</button>
+          </div>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 // export Navbar
-export default Navbar
+export default Navbar;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
 
   return (
     <form className="login" onSubmit={handleSubmit}>
-      <h3>Sign up</h3>
+      <h3>Log In</h3>
 
       <label>Email</label>
       <input
