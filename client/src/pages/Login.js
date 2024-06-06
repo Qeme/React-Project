513394a6c84540d6b284292aaea0f522cf786df4
Qeme_AX2 <qemeofficial07@gmail.com>
2024-06-06 20:45:21 +0800

    Create a useLogin hook to apply the similar logic as signup process

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isLoading, error } = useLogin();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    console.log(email, password)
-  }
+    await login(email, password);
+  };
 
   return (
     <form className="login" onSubmit={handleSubmit}>
@@ -28,7 +30,8 @@ const Login = () => {
         value={password}
       />
 
-      <button>Log In</button>
+      <button disabled={isLoading}>Log In</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
