39979b24c3731cbd1392febbf301ac6a22f2d22d
Qeme_AX2 <qemeofficial07@gmail.com>
2024-06-06 10:13:14 +0800

    Create a static function for the UserSchema to be used by userdb like userdb.create, similar to this

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,28 +1,35 @@
 // call the workouts collection
-const userdb = require('../models/userModel')
+const userdb = require("../models/userModel");
 // to handle _id format (if u use it), need to import back mongoose
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // post signup
-const signupUser = async (req,res)=>{
-    try{
-        res.status(200).json("sign up process")
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
-}
+const signupUser = async (req, res) => {
+  // take the necessary data from body
+  const { email, password } = req.body;
+
+  try {
+    // now apply the userdb.signup here
+    const user = await userdb.signup(email, password);
+
+    // pass the user information for testing
+    res.status(200).json({email, user});
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // post login
-const loginUser = async (req,res)=>{
-    try{
-        res.status(200).json("login process")
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
-}
+const loginUser = async (req, res) => {
+  try {
+    res.status(200).json("login process");
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-// export the functions 
+// export the functions
 module.exports = {
-    signupUser,
-    loginUser
-}
+  signupUser,
+  loginUser,
+};
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 // add second argument 'timestamp: true' to make sure if any new object created, the time will be recorded as well
@@ -13,6 +14,30 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+// generate static function userdb.create (.create)
+// we cant use => function because later we use 'this.'
+userSchema.statics.signup = async function (email, password) {
+  const exist = await this.findOne({ email });
+
+  if (exist) {
+    throw Error("The email already exist");
+  }
+
+  // create a salt to generate how long the hashing will be (argument)
+  // await because it took some times to generate them
+  const salt = await bcrypt.genSalt(10);
+
+  // then put the salt into hash key
+  // it took 2 arguments (the thing that you want to hash, the salt or rounds)
+  const hash = await bcrypt.hash(password, salt);
+
+  // then, we create a new user based on the email and hashed password
+  const user = await this.create({ email, password: hash });
+
+  // return the user information, for testing
+  return user;
+};
 
 // workouts is the name of the collection in DB
 // userSchema is the name of the Schema
